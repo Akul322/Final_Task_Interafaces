ccad405fef635aaf705171ecab4e7cae2208594a
Akul <artiluckashov0000@gmail.com>
2023-11-17 15:58:35 +0300

    Добавьте файлы проекта.

---
+++ Final_Task_Interafaces/Program.cs
@@ -0,0 +1,86 @@
+﻿using Final_Task_Interafaces;
+using System;
+
+namespace Final_Task_Interfaces
+{
+    class Program
+    {
+        static Ilogger Logger { get; set; }
+        static void Main()
+        {
+            ISum sum = new Summ();
+            Logger = new Logger();
+
+            var worker1 = new Worker1(Logger);
+            var worker2 = new Worker2(Logger);
+
+            try
+            {
+                Console.WriteLine("Write first number");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Write second number");
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                worker2.Work();
+
+                Console.WriteLine();
+                sum.SumMethod(a, b);
+
+                Console.WriteLine();
+                worker1.Work();
+            }
+            catch
+            {
+                Console.WriteLine("You writed incorrect value!!!");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface Ilogger
+    {
+        void Event(string message);
+
+        void Error(string message);
+    }
+    public interface ISum
+    {
+        public void SumMethod(int a, int b);
+    }
+
+    public class Summ : ISum
+    {
+        void ISum.SumMethod(int a, int b)
+        {
+            int c = a + b;
+            string str = c.ToString();
+            Console.WriteLine("Result: " + str);
+        }
+
+    }
+
+    public class Logger : Ilogger
+    {
+        void Ilogger.Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        void Ilogger.Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    public interface IWorker
+    {
+        void Work();
+    }
+}
---
+++ Final_Task_Interafaces/Worker1.cs
@@ -0,0 +1,56 @@
+﻿using Final_Task_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Task_Interafaces
+{
+    public class Worker1 : IWorker
+    {
+        Ilogger Logger { get; }
+
+        public Worker1(Ilogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Thread.Sleep(300);
+            Logger.Event("The calculator finished working!");
+            Thread.Sleep(700);
+            Logger.Error("No errors found");
+        }
+    }
+    public class Worker2 : IWorker
+    {
+        Ilogger Logger { get; }
+
+        public Worker2(Ilogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("The calculator started working");
+            Thread.Sleep(100);
+            Logger.Event("0%");
+            Thread.Sleep(300);
+            Logger.Event("22%");
+            Thread.Sleep(800);
+            Logger.Event("51%");
+            Thread.Sleep(600);
+            Logger.Event("69%");
+            Thread.Sleep(300);
+            Logger.Event("87%");
+            Thread.Sleep(2000);
+            Logger.Event("93%");
+            Thread.Sleep(700);
+            Logger.Event("100%");
+            Thread.Sleep(500);
+        }
+    }
+}
